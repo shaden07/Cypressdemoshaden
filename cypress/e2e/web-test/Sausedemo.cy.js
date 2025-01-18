005ec9ce27d113f778140681fbac35b21e0d70c9
shaden07 <106387151+shaden07@users.noreply.github.com>
2025-01-18 15:35:07 +0700

    update automation

--- cypress/e2e/web-test/Sausedemo.cy.js
+++ cypress/e2e/web-test/Sausedemo.cy.js
@@ -1,14 +1,20 @@
 describe('template spec', () => {
     it('Login Sucsess', () => {
-      cy.visit('https://www.saucedemo.com/')
-    })
-    it('Login Sucsess', () => {
         cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()    
     })
-    it('Login Sucsess', () => {
+    it('Login Failed - empty username', () => {
         cy.visit('https://www.saucedemo.com/')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
     })
-    it('Login Sucsess', () => {
+    it('Login Failed - locked user', () => {
         cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name').type('locked_out_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
     })
+    
   })
