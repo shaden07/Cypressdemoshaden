35712247c327ffbcb6db6ade808ca9100dec4aca
shaden07 <106387151+shaden07@users.noreply.github.com>
2025-01-18 15:40:40 +0700

    up verivikasi

--- cypress/e2e/web-test/Sausedemo.cy.js
+++ cypress/e2e/web-test/Sausedemo.cy.js
@@ -3,18 +3,21 @@ describe('template spec', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
-        cy.get('#login-button').click()    
+        cy.get('#login-button').click()
+        cy.get('[data-test="Eror"]').should('have.text', 'tulisane')  
     })
     it('Login Failed - empty username', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
+        cy.get('[data-test="Eror"]').should('have.text', 'tulisane')
     })
     it('Login Failed - locked user', () => {
         cy.visit('https://www.saucedemo.com/')
         cy.get('#user-name').type('locked_out_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
+        cy.get('[data-test="Eror"]').should('have.text', 'tulisane')
     })
     
   })
