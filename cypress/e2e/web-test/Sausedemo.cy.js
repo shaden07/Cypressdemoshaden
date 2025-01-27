0092e6859bbf3bbf56c3625ea27b84c54ca099b5
shaden07 <106387151+shaden07@users.noreply.github.com>
2025-01-27 23:21:01 +0700

    Update Sausedemo.cy.js

--- cypress/e2e/web-test/Sausedemo.cy.js
+++ cypress/e2e/web-test/Sausedemo.cy.js
@@ -1,10 +1,11 @@
 describe('template spec', () => {
     it('Login Sucsess', () => {
         cy.visit('https://www.saucedemo.com/')
+        cy.get('#ui-id-5').click()
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
-        cy.get('[data-test="Eror"]').should('have.text', 'tulisane')  
+        cy.get('[data-test="Eror"]').should('have.text', 'tulisane')         
     })
     it('Login Failed - empty username', () => {
         cy.visit('https://www.saucedemo.com/')
